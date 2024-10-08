b7dd80d1cb486ed42eb33a70b995b20242161ed1
Umar Yunusa <126294964+umaryun@users.noreply.github.com>
2024-10-08 17:51:13 +0100

    Update index.js documentation
    added better documentation to properly understand the code space

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+//get all possible characters
 const generateBtn = document.getElementById("generate");
-
 const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
 const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "1234567890";
@@ -13,7 +13,7 @@ generateBtn.onclick = function () {
     const allowNumbers = document.getElementById("numbers");
     const allowSpecials = document.getElementById("specialCharacters");
     let password = document.getElementById("password");
-    let allowedCharacters = "";
+    let allowedCharacters = ""; //all possible charcters for the password generation
     let generatedPassword = "";
 
     
@@ -21,6 +21,7 @@ generateBtn.onclick = function () {
         password.textContent = "Password must be at least 1(one)";
         document.getElementById("number-of-characters").value = 1;
     } else {
+        //Add the selected to the string combination of all allowed characters
         if (allowUpperCase.checked) {
             allowedCharacters += upperCaseLetters;
             allowUpperCase.checked = false;
@@ -39,11 +40,13 @@ generateBtn.onclick = function () {
         }   
         if (allowedCharacters == "") {
             password.textContent = "An option must be selected";
-        } else {
+        } else { // run once all requirements are met
             for (let i = 1; i <= Number(numberOfCharacters); i++) {
+                // add and individual random character to the result on each iteration
                 let randomIndex = Math.floor(Math.random() * allowedCharacters.length + 1);
                 generatedPassword += allowedCharacters[randomIndex];
             }
+            // display the result and reset the input to 1
             password.textContent = `Password: ${generatedPassword}`;
             document.getElementById("number-of-characters").value = 1;
         }
